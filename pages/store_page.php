85108e59c746f0696b7f2f3ccd983b56beb0521a
edoardo-morosanu <edoardomorosanu05@gmail.com>
2024-10-25 14:39:14 +0200

    removed useless file, fixed include chatbot in store_page

--- pages/store_page.php
+++ pages/store_page.php
@@ -113,7 +113,7 @@
       <div class="product-section__grid" id="product-grid"></div>
     </main>
   </div>
-  <? include '../components/chatbot.php'; ?>
+  <?php include '../components/chatbot.php'; ?>
   <?php include '../components/footer.php'; ?>
   <script src="../js/store-page.js"></script>
   <script src="../js/components.js"></script>
